620b2f76f4df99d72176d817b308302dafe75f0a
naingminkhant16 <tharkhant777@gmail.com>
2022-01-21 21:25:56 +0630

    add populate.php,auth.php,http.php

---
+++ _classes/Helpers/HTTP.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Helpers;
+
+class HTTP
+{
+    static $basic="http://localhost:8080/php_profile_pj";
+
+    static function redirect($path,$query=''){
+        $url=static::$basic . $path;
+
+        if($query)$url.="?$query";
+
+        header("location: $url");
+        exit();
+    }
+}
+
+// HTTP::redirect("_actions/populate.php",'');
